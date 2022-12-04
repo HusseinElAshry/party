2e22c29bd724a1780867deb476e8445ad8dcfe36
ashry-bey <115830798+HusseinElAshry@users.noreply.github.com>
2022-12-04 14:09:41 +0200

    Add files via upload

--- js/mine.js
+++ js/mine.js
@@ -3,13 +3,17 @@ let allp =Array.from( document.querySelectorAll(".upAndDown p"));
 let wordCounter = Number($(".counter").html());
 let localDate = new Date();
 let eventDate = new Date();
-eventDate.setFullYear(2022 , 10 , 21);
+eventDate.setFullYear(2022 , 11 , 4);
 eventDate.setHours(18 ,00,00)
 let subDate = eventDate.getTime()-localDate.getTime();
 let seconds =Math.floor(subDate/1000) ;
 let minutes= Math.floor(subDate/(1000*60));
 let hours =Math.floor(subDate/(1000*60*60));
 let days = Math.floor(subDate/(1000*60*60*24));
+let secCount;
+let minCount;
+let hCount;
+let dCount;
 $(".barIcon").css("left" , $(".navigation").outerWidth(true) + "px")
 $(".navigation").animate({left : `-${$(".navigation").outerWidth(true)}px`} ,1000)
  $(".barIcon").click(function(){
@@ -37,34 +41,50 @@ $(window).scroll(function(){
         $(".barIcon").addClass("text-white");
     }
 });
-(function secCount()
+function setCount()
 {
-    $(".time").eq(0).html(days + " D");
-    $(".time").eq(1).html(hours + " H");
-    $(".time").eq(2).html(minutes + " M");
-    $(".time").eq(3).html(seconds + " S");
-    let secCount=setInterval( function()
-    {
-        $(".time").eq(3).html( --seconds+ " S");
-    } , 1000);
-    let minCount = setInterval(function(){
-        $(".time").eq(2).html(--minutes + " M")
-    },1000*60);
-    let hCount = setInterval(function()
-    {
-        $(".time").eq(1).html(--hours + " H")
-    } , 60*60*1000);
-    let dCount = setInterval(function(){
-        $(".time").eq(0).html(--days + " D");
-    },24*60*60*1000);
-})()
-if(days==0)
+    
+         secCount=setInterval( function()
+        {
+            $(".time").eq(3).html( --seconds+ " S");
+        } , 1000);
+         minCount = setInterval(function()
+        {
+            $(".time").eq(2).html(--minutes + " M")
+        },1000*60);
+        hCount = setInterval(function()
+        {
+            $(".time").eq(1).html(--hours + " H")
+        } , 60*60*1000);
+         dCount = setInterval(function()
+         {
+            $(".time").eq(0).html(--days + " D");
+        },24*60*60*1000);
+
+}
+if(subDate<=0)
 {
     clearInterval(secCount);
     clearInterval(minCount);
     clearInterval(hCount);
     clearInterval(dCount);
+    $(".time").eq(0).html(0 + " D");
+    $(".time").eq(1).html(00 + " H");
+    $(".time").eq(2).html(00 + " M");
+    $(".time").eq(3).html(00 + " S");
+    $('.time').addClass('text-danger');
+    $('.time').removeClass('text-white');
 }
+else
+{
+    $(".time").eq(0).html(days + " D");
+    $(".time").eq(1).html(hours + " H");
+    $(".time").eq(2).html(minutes + " M");
+    $(".time").eq(3).html(seconds + " S");
+
+    setCount()
+}
+
 $("textarea").keydown(function()
 {
     if(wordCounter==0)
